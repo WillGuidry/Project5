896d2a15a2ef88b102ceaa0bd068756ccd89fe1c
samanthaz123 <103225744+samanthaz123@users.noreply.github.com>
2022-11-12 22:47:58 -0500

    Update EngagementData.java

--- src/popularity/EngagementData.java
+++ src/popularity/EngagementData.java
@@ -1,5 +1,23 @@
 package popularity;
 
 public class EngagementData {
+  
+  private EngagementDLList<String> month;
+  private EngagementDLList<String> username;
+  private EngagementDLList<String> channelName;
+  private EngagementDLList<String> country;
+  private EngagementDLList<String> mainTopic;
+  private EngagementDLList<int> likes;
+  private EngagementDLList<int> posts;
+  private EngagementDLList<int> followers;
+  private EngagementDLList<int> comments;
+  private EngagementDLList<int> views;
+  
+  public EngagemetData(EngagementDLList<String> month, EngagementDLList<String> username, EngagementDLList<String> channelName, EngagementDLList<String> channelName,
+                       EngagementDLList<String> country, EngagementDLList<String> mainTopic, EngagementDLList<int> likes, EngagementDLList<int> posts, EngagementDLList<int> followers,
+                       EngagementDLList<int> comments, EngagementDLList<int> views);
+  
+                       
+    
 
 }
