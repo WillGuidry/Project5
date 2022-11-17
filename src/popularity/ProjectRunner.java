381266a562021730916b70fcc5edd91f925e0957
samanthaz123 <103225744+samanthaz123@users.noreply.github.com>
2022-11-17 01:52:19 -0500

    Update ProjectRunner.java

--- src/popularity/ProjectRunner.java
+++ src/popularity/ProjectRunner.java
@@ -1,5 +1,13 @@
 package popularity;
 
+import java.io.FileNotFoundException;
+import java.text.ParseException;
+
 public class ProjectRunner {
 
+    public static void main(String[] args) throws FileNotFoundException, ParseException {
+        ReadInput input = new ReadInput("sampleInput1.txt");
+        input.printOutput2();
+        
+    }
 }
